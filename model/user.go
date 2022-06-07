bfbb50df91cbe4485fb21368208d528041df39ac
implank <tense_chen@outlook.com>
2022-06-08 00:34:13 +0800

    chore

--- model/user.go
+++ model/user.go
@@ -4,14 +4,14 @@ import "time"
 
 type User struct {
 	UserID    uint64    `gorm:"primary_key" json:"user_id"`
-	Username  string    `gorm:"size:255 not null" json:"user_name"`
-	Password  string    `gorm:"size:255 not null" json:"password"`
-	Email     string    `gorm:"size:255 not null" json:"email"`
-	UserLevel uint64    `gorm:"not null default:1" json:"user_level"`
-	Exp       uint64    `gorm:"not null default:0" json:"exp"`
+	Username  string    `gorm:"size:255;not null" json:"user_name"`
+	Password  string    `gorm:"size:255;not null" json:"password"`
+	Email     string    `gorm:"size:255;not null" json:"email"`
+	UserLevel uint64    `gorm:"not null;default:1" json:"user_level"`
+	Exp       uint64    `gorm:"not null;default:0" json:"exp"`
 	Ban       bool      `gorm:"default:false" json:"ban"`
 	Bandate   time.Time `gorm:"default:Now()" json:"bandate"`
-	Sex       string    `gorm:"type:varchar(255)" json:"sex"`
+	Sex       string    `gorm:"size:255" json:"sex"`
 	Age       uint64    `gorm:"default:0" json:"age"`
 	AvatarUrl string    `gorm:"default:null" json:"avatar_url"`
 }
