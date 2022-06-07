a737e3767815c214d6f8d859683b8848ca7c33b5
implank <tense_chen@outlook.com>
2022-06-07 19:11:25 +0800

    fix:user default level

--- model/user.go
+++ model/user.go
@@ -7,7 +7,7 @@ type User struct {
 	Username  string    `gorm:"size:255 not null" json:"user_name"`
 	Password  string    `gorm:"size:255 not null" json:"password"`
 	Email     string    `gorm:"size:255 not null" json:"email"`
-	UserLevel uint64    `gorm:"not null default:0" json:"user_level"`
+	UserLevel uint64    `gorm:"not null default:1" json:"user_level"`
 	Exp       uint64    `gorm:"not null default:0" json:"exp"`
 	Ban       bool      `gorm:"default:false" json:"ban"`
 	Bandate   time.Time `gorm:"default:Now()" json:"bandate"`
